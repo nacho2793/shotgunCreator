0399fce0af27cf998605b8b8dc965874660255c6
Unknown <0163473@up.edu.mx>
2017-06-10 09:56:16 -0500

    Correct Bugs

--- Pipeline.py
+++ Pipeline.py
@@ -8,7 +8,6 @@ import os.path
 
 print Shotgun
 
-#sg = Shotgun("https://upgdl.shotgunstudio.com", login="ignacho", password="Minions2793" )
 sg = Shotgun("https://upgdl.shotgunstudio.com", "NachoScript", "486c9aa2bf63e4a83f975e3928207342e15dfcfeb2066384d7e48f9da7087923" )
 
 #Asking user:
@@ -18,7 +17,7 @@ validation = False
 typeToUpload = raw_input("What do you want to upload: Shoot o Asset? (S/A)\n").lower()
 while validation==False:
 	typeToUpload = typeToUpload.lower()
-	if (typeToUpload=='s') or (typeToUpload=='a'):
+	if (typeToUpload=='s'):
 		validation=True
 		typeToUpload = 'Shot'
 	elif typeToUpload=='a':
@@ -30,6 +29,7 @@ while validation==False:
 """Define the ID of Asset or Shot"""
 validId = False
 validRequest = False
+validation = False
 ID = raw_input("Type the id of the %s:\n" % typeToUpload)
 while validRequest==False:
 	#Number Validation
@@ -41,24 +41,29 @@ while validRequest==False:
 			print "Invalid input, the id must be a Number"
 			ID = raw_input("Type the CORRECT id of the Asset:\n")
 	#Shotgun Validation
-	try:
-		shoot ={'id': ID, 'type': typeToUpload}
-		filters = [['entity','is', shoot]]
-		fields = ['code']
-		versions = sg.find("Version", filters, fields)
-		if len(versions)==0:
-			print "The ID doesnt exist in Shotgun"
-			ID = raw_input("Type the CORRECT id of the Asset:\n")
-			validId= False
-		else:
-			print "The ID %s have this versions:\n" %ID
+	
+	shoot ={'id': ID, 'type': typeToUpload}
+	filters = [['entity','is', shoot]]
+	fields = ['code']
+	versions = sg.find_one(typeToUpload, [["id", "is", ID]], ["id", "code", "sg_status_list"])
+		
+	if versions == None:
+		print "The ID doesnt exist in Shotgun"
+		ID = raw_input("Type the CORRECT id of the Asset:\n")
+		validId = False
+	else:
+		print "%s name: %s" %(typeToUpload, versions['code'])
+		try:
+			versions = sg.find("Version", filters, fields)
+		except Exception as e:
+			print e
+		if len(versions)>0:
+			print "The %s have this versions already:\n" %typeToUpload
 			for version in versions:
 				print "  -%s" %version["code"]
-			validRequest=True
-	except:
-		print "EXCEPTION:The ID doesnt exist in Shotgun"
-		ID = raw_input("Type the CORRECT id of the Asset:\n")
-"""Naming the Version"""
+		else:
+			print "No versions created in this %s" %typeToUpload
+		validRequest=True
 raw_name = raw_input("Enter a name for a version (without vXXX):\n")
 final_name = ''
 for version in versions:
@@ -75,15 +80,10 @@ description = raw_input("Type a description for your new version:\n")
 create = False
 file_path = raw_input("Add the path of the file to upload:\n")
 while create==False:
-	try:
-		if os.path.isfile(file_path) :
-			create=True
-		else:
-			file_path = raw_input("Add the CORRECT path of the file to upload:\n")
-		
-	except Exception as e:
-		print e
-		time.sleep(10)
+	if os.path.isfile(file_path) :
+		create=True
+	else:
+		file_path = raw_input("Add the CORRECT path of the file to upload (remember to add extension):\n")
 data = {
 'code': final_name,
 'entity': {'id': ID, 'type':typeToUpload},
@@ -94,9 +94,10 @@ data = {
 'project': {'id':110, 'type':'Project'}}
 try:
 	result = sg.create("Version", data)
+	print "Creating new version of the %s %s" %(typeToUpload, ID)
 #file_path = 'C:\\Users\\Nacho\\Pictures\\turtle.jpg'
-	print sg.upload("Version", result["id"], file_path, field_name="sg_uploaded_movie", display_name="Media")
+	print "The new version have the ID: %s" %sg.upload("Version", result["id"], file_path, field_name="sg_uploaded_movie", display_name="Media")
 except Exception as e:
 	print e
 print "Version added Succesfully. Bye!"
-time.sleep(15)
+time.sleep(5)
